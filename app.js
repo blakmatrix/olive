8e67f259465d79fa692d0488307f785d1f02d3ab
Farrin Reid <blakmatrix@koteako.com>
2011-07-22 19:17:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer()
+   , io = require('socket.io').listen(app);
+
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+// Error Handling
+
+//process.on('uncaughtException', function(e) {
+//    sys.log(e.stack);
+//});
+
+//process.on('SIGINT', function() {
+//    if (socket) socket.broadcast({ type: 'restart' });
+//    setTimeout(function() {
+//        process.exit();
+//    }, 2000);
+//});
+var g_color = '808000';
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'OLIVE',
+    g_color: "body{background-color:#"+g_color+";}"
+  });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', {
+    title: 'OLIVE login',
+    g_color: "body{background-color:#"+g_color+";}"
+  });
+});
+
+app.get('/colorpicker', function(req, res){
+  res.render('colorpicker', {
+    title: 'OLIVE color picker',
+    g_color: "body{background-color:#"+g_color+";}"
+  });
+});
+
+// Model
+var bcs = [{hex_val: "808000", color_name: "OLIVE (web color)"},
+           {hex_val: "ADFF2F", color_name: "Green-Yellow"},
+           {hex_val: "7CFC00", color_name: "Lawn Green"},
+           {hex_val: "93C572", color_name: "Pistachio"},
+           {hex_val: "9ACD32", color_name: "Yellow-Green"},
+           {hex_val: "8DB600", color_name: "Apple Green"},
+           {hex_val: "9AB973", color_name: "Olive Green"},
+           {hex_val: "808000", color_name: "Olive"},
+           {hex_val: "6B8E23", color_name: "Olive Drab"},
+           {hex_val: "78866B", color_name: "Camouflage Green"},
+           {hex_val: "556B2F", color_name: "Dark Olive Green"}
+];
+//var users = require('./users');
+//var USERS = users.USERS;
+//var User = users.User;
+
+// IO
+
+
+// listen
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+//IO
+
+io.sockets.on('connection', function (socket) {
+
+  socket.emit('color_change', { color: g_color });
+  socket.on('set_color', function (data) {
+    console.log(data);
+    g_color=data.change_color;
+    socket.broadcast.emit('update_color', { color: data.change_color });
+  });
+});
---
+++ public/javascripts/sliderstuff.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+  
+  $("#content-slider").slider({
+    animate: true,
+    change: handleSliderChange,
+    slide: handleSliderSlide
+  });
+
+});
+
+
+
+function setWindowColor()
+{   
+    var socket = io.connect('http://192.168.1.116:3000');
+    color = getColor("div#color_box");
+    $("body").css("background-color","#"+color);
+    socket.emit('set_color', {change_color: color });
+    
+}
+
+function setColor(a, color)
+{   
+    $(a).css("background-color","#"+color); 
+}
+function getColor(a)
+{   
+    var color_rgb= $(a).css("background-color");
+    var color = "";
+    if(color_rgb.match(/^rgba/)){
+      color_rgb = color_rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+      // parts now should be ["rgba(0, 70, 255, 0)", "0", "70", "255"]
+      var R=color_rgb[1];
+      var G=color_rgb[2];
+      var B=color_rgb[3];
+      var color = rgbToHex(R,G,B);
+    }else if(color_rgb.match(/^rgb/)){
+      color_rgb = color_rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+      // parts now should be ["rgb(0, 70, 255", "0", "70", "255"]
+      var R=color_rgb[1];
+      var G=color_rgb[2];
+      var B=color_rgb[3];
+      var color = rgbToHex(R,G,B);
+    }else if(color_rgb.match(/^#/)){
+         color = color_rgb.replace(/#/g,'');
+    }else{
+        color = "808000";
+    }
+    return color; 
+    
+}
+
+function handleSliderChange(e, ui)
+{
+  //var maxScroll = $("#content-scroll").attr("scrollWidth") - $("#content-scroll").width();
+  //$("#content-scroll").animate({scrollLeft: ui.value * (maxScroll / 100) }, 1000);
+  $(function(){ChangeColor("#color_box", ui.value)});
+}
+
+function handleSliderSlide(e, ui)
+{
+  //var maxScroll = $("#content-scroll").attr("scrollWidth") - $("#content-scroll").width();
+  //$("#content-scroll").attr({scrollLeft: ui.value * (maxScroll / 100) });
+  $(function(){ChangeColor("#color_box", ui.value)});
+}
+
+function hsv2rgb(d,g){
+    var c=(d.H>=0&&d.H<360)?d.H:0;
+    var l=(d.S>=0&&d.S<=1)?d.S:1;
+    var j=(d.V>=0&&d.V<=255)?d.V:255;
+    var i=Math.floor(c/60);
+    var e=c/60-i;
+    var b=Math.round(j*(1-l));
+    var a=Math.round(j*(1-e*l));
+    var k=Math.round(j*(1-(1-e)*l));
+    switch(i){
+        case 0:g.R=j,g.G=k,g.B=b;break;
+        case 1:g.R=a,g.G=j,g.B=b;break;
+        case 2:g.R=b,g.G=j,g.B=k;break;
+        case 3:g.R=b,g.G=a,g.B=j;break;
+        case 4:g.R=k,g.G=b,g.B=j;break;
+        case 5:g.R=j,g.G=b,g.B=a;break
+    }
+}
+
+function ChangeColor(a, pos){
+    var f={R:0,G:0,B:0};
+    //H 180-60
+    // V 240 - 0
+    n_pos = pos;
+    if(pos == 100){n_pos=99;}
+    var hue_range = 160-60;
+    var hue_increment = ((n_pos%10)/10)*hue_range;
+    var hue_val = 60+hue_increment;
+    var value_increment = ((Math.floor(n_pos/10)%10)/10)*240;
+    var lum_val = 240-value_increment;
+    var e={H:hue_val,S:0.95,V:lum_val};
+
+    hsv2rgb(e,f);
+
+    $(a).css("background-color","rgb("+f.R+","+f.G+","+f.B+")");
+    $("#slider_val").text("slider=  "+pos);
+    $("#hue_val").text("hue val= "+hue_val);
+    $("#lum_val").text("lum val= "+lum_val);
+    var hex_val = rgbToHex(f.R, f.G, f.B)
+    $("#hex_val").text("hex val= #"+hex_val);
+    setColor(a,hex_val);
+    //body_color_str = hex_val;
+    //$("#bcs_val").text("bcs val= #"+body_color_str);
+}
+function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
+function toHex(n) {
+ n = parseInt(n,10);
+ if (isNaN(n)) return "00";
+ n = Math.max(0,Math.min(n,255));
+ return "0123456789ABCDEF".charAt((n-n%16)/16)
+      + "0123456789ABCDEF".charAt(n%16);
+}
+
